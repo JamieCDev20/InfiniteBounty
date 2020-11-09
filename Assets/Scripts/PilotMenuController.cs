ddcb539d4577580aa90a2fa31ec1b47b026ce2ed
Nick-Furniss <44497325+Nick-Furniss@users.noreply.github.com>
2020-11-09 15:24:03 +0000

    #113 Pushing for Jame

---
+++ Assets/Scripts/PilotMenuController.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PilotMenuController : MonoBehaviour
+{
+    #region Gamemodes
+
+    public void SelectedNuggetRun()
+    {
+        print("Should've selected Nugget Run");
+    }
+
+    public void SelectedMotherlode()
+    {
+        print("Should've selected Motherlode");
+    }
+
+    #endregion
+
+    #region Planet buttons
+
+    public void ChooseIgnova()
+    {
+        print("Should've selected Ignova");
+    }
+
+    #endregion
+
+}
