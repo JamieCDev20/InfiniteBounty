1d9c95624188b5ff9f32ec9f6a24445cd7423ba5
Nick-Furniss <44497325+Nick-Furniss@users.noreply.github.com>
2020-11-09 16:27:24 +0000

    Fixed a thing with the main menu

--- Assets/Scripts/MainMenuController.cs
+++ Assets/Scripts/MainMenuController.cs
@@ -1,9 +1,26 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class MainMenuController : MonoBehaviour
 {
+
+    [SerializeField] private Button b_onlineButton;
+
+    private void Start()
+    {
+        b_onlineButton.interactable = false;
+        Invoke("EnableButtons", 2);
+    }
+
+    internal void EnableButtons()
+    {
+        Debug.LogError("Did this get called when the server was reached? Probably not.");
+        b_onlineButton.interactable = true;
+    }
+
+
     public void Quit()
     {
 #if UNITY_EDITOR
