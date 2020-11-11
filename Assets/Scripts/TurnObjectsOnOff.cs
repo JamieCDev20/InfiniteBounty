21a2eaa68abd5e4a2fef6dad3a3a85b611361015
Nick-Furniss <44497325+Nick-Furniss@users.noreply.github.com>
2020-11-11 15:23:52 +0000

    #117 Path Maker

---
+++ Assets/Scripts/TurnObjectsOnOff.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TurnObjectsOnOff : MonoBehaviour
+{
+    [Header("Object References")]
+    [SerializeField] private PathBlockades[] pbA_blockades = new PathBlockades[0];
+
+    private void Start()
+    {
+        for (int i = 0; i < pbA_blockades.Length; i++)
+        {
+            pbA_blockades[i].goA_Obstacles[Random.Range(0, pbA_blockades[i].goA_Obstacles.Length)].SetActive(false);
+        }
+    }
+}
+
+[System.Serializable]
+public struct PathBlockades
+{
+    public GameObject[] goA_Obstacles;
+}
