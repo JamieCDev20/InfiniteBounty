c8d3ae863157647093ec9864941acae98319bf0b
Nick-Furniss <44497325+Nick-Furniss@users.noreply.github.com>
2020-11-16 09:43:28 +0000

    Music?

---
+++ Assets/Scripts/MusicStarter.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MusicStarter : MonoBehaviour
+{
+    [SerializeField] private AudioClip ac_music;
+
+    void Start()
+    {
+        GetComponent<AudioSource>().PlayOneShot(ac_music);
+    }
+}
