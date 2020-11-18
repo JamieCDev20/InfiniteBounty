7d4738b263e7e319c20409489221857e67c65314
Jamboc731 <jamiec.covellj@gmail.com>
2020-11-18 17:40:45 +0000

    My changes for the day

---
+++ Assets/Scripts/GOAP/NewGoap/NGoapAgent.cs
@@ -0,0 +1,87 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NGoapAgent : MonoBehaviour
+{
+
+    //Variables
+    #region Serialised
+    [Header("Movement")]
+    [SerializeField] private AIMovementType movementType;
+    [SerializeField] private AIMovementStats movementStats;
+
+    #endregion
+
+    #region Private
+
+    private AIGroundMover mover;
+
+    #endregion
+
+    //Methods
+    #region Unity Standards
+
+    private void Start()
+    {
+        Init();
+    }
+
+    private void FixedUpdate()
+    {
+        mover.Move();
+    }
+
+    #endregion
+
+    #region Private Voids
+
+    private void Init()
+    {
+        switch (movementType)
+        {
+            case AIMovementType.ground:
+                mover = new AIGroundMover(movementStats);
+                mover.SetRB(GetComponent<Rigidbody>());
+                break;
+            case AIMovementType.air:
+                break;
+            default:
+                break;
+        }
+    }
+
+    #endregion
+
+    #region Public Voids
+
+    //public void SetTarget()
+
+    #endregion
+
+    #region Private Returns
+
+
+    #endregion
+
+    #region Public Returns
+
+
+    #endregion
+
+}
+
+public enum AIMovementType
+{
+    ground,
+    air
+}
+
+[System.Serializable]
+public struct AIMovementStats
+{
+    public float f_movementSpeed;
+    public float f_jumpHeight;
+    public Vector3 v_drag;
+
+}
---
+++ Assets/Scripts/TempScripts/TempAI/TempTargetter.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TempTargetter : MonoBehaviour
+{
+
+    //Variables
+    #region Serialised
+
+    [SerializeField] private NGoapAgent agent;
+    [SerializeField] private Transform target;
+
+    public bool transformFollow;
+
+    #endregion
+
+    #region Private
+
+
+    #endregion
+
+    //Methods
+    #region Unity Standards
+
+    private void FixedUpdate()
+    {
+        if (transformFollow)
+        {
+            //agent.
+        }
+    }
+
+    #endregion
+
+    #region Private Voids
+
+
+    #endregion
+
+    #region Public Voids
+
+
+    #endregion
+
+    #region Private Returns
+
+
+    #endregion
+
+    #region Public Returns
+
+
+    #endregion
+
+}
