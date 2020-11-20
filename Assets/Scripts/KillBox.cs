c55bfa1236877a718e19578e51dadb9575738799
NICK FURNISS <nf217269@falmouth.ac.uk>
2020-11-20 10:03:20 +0000

    Bullets can kill the player?

---
+++ Assets/Scripts/KillBox.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KillBox : MonoBehaviour
+{
+    [Header("Movement")]
+    [SerializeField] private Vector3 v_unitsPerSecond;
+
+
+    private void Update()
+    {
+        transform.position += v_unitsPerSecond;
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            other.GetComponent<PlayerHealth>().TakeDamage(10000);
+        }
+    }
+
+}
