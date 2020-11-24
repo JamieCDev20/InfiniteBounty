b0ec82f7876d2ff8b50cdaabbad2443f64c1b002
Jamboc731 <jamiec.covellj@gmail.com>
2020-11-24 14:04:52 +0000

    Update LodeBase.cs

--- Assets/Scripts/Lodes/LodeBase.cs
+++ Assets/Scripts/Lodes/LodeBase.cs
@@ -144,7 +144,7 @@ public class LodeBase : Enemy, IPoolable, IPunObservable, IHitable
             Vector3 v = Vector3.zero;
             for (int i = 0; i < stream.Count; i++)
             {
-                string[] t = ((string)stream.ReceiveNext()).Split('#');
+                string[] t = (stream.ReceiveNext().ToString()).Split('#');
                 string[] tA = t[1].Split(',');
                 v.x = float.Parse(tA[0]);
                 v.y = float.Parse(tA[1]);
