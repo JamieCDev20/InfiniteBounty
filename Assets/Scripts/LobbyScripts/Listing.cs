c8c85889b3f62aac167e114703ad09e0154d79cb
Jamboc731 <jamiec.covellj@gmail.com>
2020-11-24 11:34:36 +0000

    all my lobby testing changes

---
+++ Assets/Scripts/LobbyScripts/Listing.cs
@@ -0,0 +1,24 @@
+﻿using Photon.Realtime;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Listing : MonoBehaviour
+{
+
+    [SerializeField] private Text t_titleText;
+    [SerializeField] private Text t_playerCountText;
+
+    public void SetInfo(RoomInfo info)
+    {
+        t_titleText.text = info.Name;
+        t_playerCountText.text = $"{info.PlayerCount}/{info.MaxPlayers}";
+    }
+
+    public void Destroy()
+    {
+        Destroy(gameObject);
+    }
+
+}
---
+++ Assets/Scripts/LobbyScripts/Lobby.cs
@@ -0,0 +1,115 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Pun;
+using UnityEngine.UI;
+using Photon.Realtime;
+
+public class Lobby : MonoBehaviourPunCallbacks
+{
+
+    [SerializeField] private InputField if_gameTitleInput;
+    [SerializeField] private Scrollbar sb_bar;
+    [SerializeField] private GameObject go_roomListing;
+    [SerializeField] private Transform t_roomListParent;
+    
+    private List<Listing> goL_listings = new List<Listing>();
+
+    private void Start()
+    {
+        sb_bar.value = 0;
+        PhotonNetwork.ConnectUsingSettings();
+    }
+
+    public override void OnConnectedToMaster()
+    {
+        Debug.Log("Connected");
+        PhotonNetwork.JoinLobby(TypedLobby.Default);
+    }
+
+    public override void OnJoinedLobby()
+    {
+        Debug.Log("joined lobby");
+        //PhotonNetwork.JoinOrCreateRoom("roomname", new RoomOptions() { MaxPlayers = 4 }, TypedLobby.Default);
+    }
+
+    public override void OnJoinedRoom()
+    {
+        Debug.Log("Joined a room!!");
+    }
+
+    public void OnClickJoin()
+    {
+
+    }
+
+    public void OnClickCreate()
+    {
+
+    }
+
+    public override void OnRoomListUpdate(List<RoomInfo> roomList)
+    {
+        base.OnRoomListUpdate(roomList);
+        Debug.Log("room list update : " + roomList.Count);
+        UpdateRoomList(roomList);
+
+    }
+
+    private void UpdateRoomList(List<RoomInfo> roomList)
+    {
+
+        if (roomList.Count > goL_listings.Count)
+        {
+            for (int i = 0; i < roomList.Count; i++)
+            {
+                if (roomList[i].IsVisible)
+                {
+                    if (i < goL_listings.Count)
+                    {
+                        goL_listings[i].SetInfo(roomList[i]);
+                    }
+                    else
+                    {
+                        GameObject l = Instantiate(go_roomListing, t_roomListParent);
+                        goL_listings.Add(l.GetComponent<Listing>());
+                        goL_listings[goL_listings.Count - 1].SetInfo(roomList[i]);
+                    }
+                }
+            }
+        }
+        else
+        {
+            for (int i = 0; i < goL_listings.Count; i++)
+            {
+                if (i < roomList.Count)
+                {
+                    if (roomList[i].IsVisible)
+                        goL_listings[i].SetInfo(roomList[i]);
+                    else
+                    {
+                        goL_listings[i].Destroy();
+                        goL_listings[i] = null;
+                    }
+                }
+                else
+                {
+                    goL_listings[i].Destroy();
+                    goL_listings[i] = null;
+                }
+            }
+        }
+
+        RemoveAllNullFromList(goL_listings);
+    }
+
+    private void RemoveAllNullFromList(List<Listing> list)
+    {
+        for (int i = list.Count - 1; i > 0; i++)
+        {
+            if (list[i] == null)
+                list.RemoveAt(i);
+        }
+    }
+
+}
