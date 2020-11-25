b061f10b36942c8617a00147c3e2fe6714da2394
Jamboc731 <jamiec.covellj@gmail.com>
2020-11-25 10:03:01 +0000

    Update ToolBase.cs

--- Assets/Scripts/Tools/ToolBase.cs
+++ Assets/Scripts/Tools/ToolBase.cs
@@ -39,18 +39,6 @@ public abstract class ToolBase : MonoBehaviourPun, IPurchasable
 
     #endregion
 
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
-
     public virtual void Use()
     {
 
