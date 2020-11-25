52046ec7c76523f82ea8bec617edad0223664442
Byron Crowhurst <57679765+ByPossum@users.noreply.github.com>
2020-11-25 13:47:35 +0000

    Update NugSucker.cs

--- Assets/Scripts/Tools/Cone Tools/NugSucker.cs
+++ Assets/Scripts/Tools/Cone Tools/NugSucker.cs
@@ -7,6 +7,9 @@ public class NugSucker : ConeTool
     [SerializeField] private float f_suckForce;
     public override void Use(Vector3 _v_forwards)
     {
+        ParticleSystem.ShapeModule sm = go_particles.GetComponentInChildren<ParticleSystem>().shape;
+        sm.angle = f_angle * 0.5f;
+        Debug.Log(sm.angle);
         PlayParticles(true);
         foreach(GameObject hit in GetAllObjectsInCone(_v_forwards))
         {
