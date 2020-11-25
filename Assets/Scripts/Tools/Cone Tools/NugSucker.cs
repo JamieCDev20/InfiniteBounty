f8699044b63c387c31f829cdfc7865c12ebde08b
Byron Crowhurst <57679765+ByPossum@users.noreply.github.com>
2020-11-25 12:24:04 +0000

    #183 Nuhg ver based on For Wards

--- Assets/Scripts/Tools/Cone Tools/NugSucker.cs
+++ Assets/Scripts/Tools/Cone Tools/NugSucker.cs
@@ -7,7 +7,8 @@ public class NugSucker : ConeTool
     [SerializeField] private float f_suckForce;
     public override void Use(Vector3 _v_forwards)
     {
-        foreach(GameObject hit in GetAllObjectsInCone())
+        PlayParticles(true);
+        foreach(GameObject hit in GetAllObjectsInCone(_v_forwards))
         {
             hit.GetComponent<ISuckable>()?.GetRigidbody().AddForce((t_conePoint.position - hit.transform.position).normalized * f_suckForce, ForceMode.Impulse);
         }
