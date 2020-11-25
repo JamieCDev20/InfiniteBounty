adc8c24db88587dd56b95cdc5d05c96476893622
Nick-Furniss <44497325+Nick-Furniss@users.noreply.github.com>
2020-11-25 11:36:10 +0000

    More jetpack tuning

--- Assets/Scripts/Tools/Mobility/Jetpack.cs
+++ Assets/Scripts/Tools/Mobility/Jetpack.cs
@@ -8,7 +8,7 @@ public class Jetpack : MobilityTool
     [SerializeField] private float f_force = 5;
     private Rigidbody rb;
     [SerializeField] private AnimationCurve ac_jetPackForce;
-    private float f_timeHeld;
+    [Space, SerializeField] private float f_timeHeld;
 
     private void Start()
     {
