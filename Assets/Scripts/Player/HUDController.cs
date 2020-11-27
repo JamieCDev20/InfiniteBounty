ec41e20e74720f6274c8991ece0328845d240139
Nick-Furniss <44497325+Nick-Furniss@users.noreply.github.com>
2020-11-27 14:50:32 +0000

    New health bar

--- Assets/Scripts/Player/HUDController.cs
+++ Assets/Scripts/Player/HUDController.cs
@@ -6,8 +6,8 @@ using UnityEngine.UI;
 public class HUDController : MonoBehaviour
 {
     [Header("Heat Guages")]
-    [SerializeField] private RectTransform rt_leftHeatGuage;
-    [SerializeField] private RectTransform rt_rightHeatGuage;
+    [SerializeField] private RectTransform rt_healthBar;
+    //[SerializeField] private RectTransform rt_rightHeatGuage;
 
     [Header("Health Stats")]
     [SerializeField] private List<Sprite> sA_faceSprites = new List<Sprite>();
@@ -18,25 +18,27 @@ public class HUDController : MonoBehaviour
     private void Start()
     {
         SetHealthBarValue(1, 1);
-        SetLeftHeatGuage(1, 1);
-        SetRightHeatGuage(1, 1);
+        //SetLeftHeatGuage(1, 1);
+        //SetRightHeatGuage(1, 1);
     }
 
     public void SetHealthBarValue(float _i_currentHealth, int _i_maxHealth)
     {
         //print((float)_i_currentHealth / _i_maxHealth + "/" + Mathf.RoundToInt(((float)_i_currentHealth / _i_maxHealth) * sA_faceSprites.Count));
-        i_faceBackgroundImage.color = g_healthBarGradient.Evaluate((float)_i_currentHealth / _i_maxHealth);
+        //i_faceBackgroundImage.color = g_healthBarGradient.Evaluate((float)_i_currentHealth / _i_maxHealth);
+
+        rt_healthBar.localScale = new Vector3((_i_currentHealth / _i_currentHealth), 1, 1);
         i_faceImage.sprite = sA_faceSprites[Mathf.Clamp(Mathf.RoundToInt(((float)_i_currentHealth / _i_maxHealth) * sA_faceSprites.Count), 0, 4)];
     }
 
     public void SetLeftHeatGuage(int _i_currentHeat, int _i_maxHeat)
     {
-        rt_leftHeatGuage.localScale = new Vector3(1 - (_i_currentHeat / _i_maxHeat), 1, 1);
+        rt_healthBar.localScale = new Vector3(1 - (_i_currentHeat / _i_maxHeat), 1, 1);
     }
 
     public void SetRightHeatGuage(int _i_currentHeat, int _i_maxHeat)
     {
-        rt_rightHeatGuage.localScale = new Vector3(1 - (_i_currentHeat / _i_maxHeat), 1, 1);
+        //rt_rightHeatGuage.localScale = new Vector3(1 - (_i_currentHeat / _i_maxHeat), 1, 1);
     }
 
 
