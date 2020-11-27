769cd511762cb93ad9006e2fb766b2c04e068d20
Nick-Furniss <44497325+Nick-Furniss@users.noreply.github.com>
2020-11-27 11:54:36 +0000

    PlayerName

---
+++ Assets/Scripts/Bluetack/Billboard.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Billboard : MonoBehaviour
+{
+    private Camera c_cam;
+
+    private void Start()
+    {
+        c_cam = Camera.main;
+
+        if (GetComponentInParent<PlayerInputManager>().GetID() == NetworkedPlayer.x.PlayerID)
+            gameObject.SetActive(false);
+
+    }
+
+    void Update()
+    {
+        transform.LookAt(c_cam.transform);
+    }
+}
