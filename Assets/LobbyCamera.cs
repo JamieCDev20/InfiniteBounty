3a4a5151f90e546caba492c916e5a3ac5182e789
johnarthurmeakin <johnnyjam23@hotmail.com>
2020-12-11 11:23:53 +0000

    Edited the lobby camera and scripts

---
+++ Assets/LobbyCamera.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LobbyCamera : MonoBehaviour
+{
+
+    public void SwitchToLobbyCam(Transform _t_newPos)
+    {
+        gameObject.transform.position = _t_newPos.position;
+        gameObject.SetActive(true);
+        NetworkedPlayer.x.SetCameraActive(false);
+    }
+
+    public void SwitchToPlayerCam()
+    {
+        NetworkedPlayer.x.SetCameraActive(true);
+        gameObject.SetActive(false);
+    }
+}
