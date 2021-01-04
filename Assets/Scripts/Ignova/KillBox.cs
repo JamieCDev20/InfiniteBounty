5a54d52d0eef7814dfc9ed47ada53e1db3f9b160
Nick-Furniss <44497325+Nick-Furniss@users.noreply.github.com>
2021-01-04 11:01:28 +0000

    Ash cloud updates
    Now deals 10 damage every 0.1 seconds

--- Assets/Scripts/Ignova/KillBox.cs
+++ Assets/Scripts/Ignova/KillBox.cs
@@ -7,6 +7,10 @@ public class KillBox : MonoBehaviour
     [Header("Movement")]
     [SerializeField] private Vector3 v_unitsPerSecond;
     [SerializeField] private bool b_shouldSinBackToStart;
+    [SerializeField] private int i_damageToDeal;
+    private List<IHitable> hL_thingsWithinCloud = new List<IHitable>();
+    private float f_time;
+    [SerializeField] private float f_timeBetweenDamages;
 
     private void Update()
     {
@@ -14,13 +18,34 @@ public class KillBox : MonoBehaviour
             transform.position += (v_unitsPerSecond * Mathf.Sin(Time.realtimeSinceStartup));
         else
             transform.position += v_unitsPerSecond * Time.deltaTime;
+
+        f_time += Time.deltaTime;
+        if (f_time >= f_timeBetweenDamages)
+            DealDamage();
+
     }
 
     private void OnTriggerEnter(Collider other)
     {
-        //other.GetComponent<PlayerHealth>().TakeDamage(10000);
-        other.GetComponent<IHitable>()?.TakeDamage(10000);
+        IHitable _h = other.GetComponent<IHitable>();
 
+        if (_h != null)
+            hL_thingsWithinCloud.Add(_h);
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        IHitable _h = other.GetComponent<IHitable>();
+
+        if (hL_thingsWithinCloud.Contains(_h))
+            hL_thingsWithinCloud.Remove(_h);
+    }
+
+    private void DealDamage()
+    {
+        for (int i = 0; i < hL_thingsWithinCloud.Count; i++)
+            hL_thingsWithinCloud[i].TakeDamage(i_damageToDeal);
+        f_time = 0;
     }
 
 }
