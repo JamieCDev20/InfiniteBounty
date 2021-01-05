5328cc7773cba6ebeff0767c980e256bd3a66844
Nick-Furniss <44497325+Nick-Furniss@users.noreply.github.com>
2021-01-05 12:12:40 +0000

    Enemy spawner upgrades

--- Assets/Scripts/Ignova/EnemySpawner.cs
+++ Assets/Scripts/Ignova/EnemySpawner.cs
@@ -7,16 +7,17 @@ public class EnemySpawner : MonoBehaviourPunCallbacks
 {
     public static EnemySpawner x;
 
-    [SerializeField] private GameObject[] goA_enemiesToSpawnAtStart;
-    [SerializeField] private GameObject[] goA_enemiesToSpawnDuringAWave;
     [SerializeField] private Transform[] tA_spawnPoints;
+    [SerializeField] private GameObject[] goA_enemiesToSpawnAtStart;
+    [Space, SerializeField] private GameObject[] goA_enemiesToSpawnDuringAWave;
+    [SerializeField] private float[] fA_enemyPerWaveWeighting;
     //[SerializeField] private float f_timeBetweenSpawns = 3;
 
     private float spawnCount;
     [Header("Spawn Rate")]
     [SerializeField] private int i_enemiesAtStart;
-    [SerializeField] private int i_secondsBetweenWave;
-    [SerializeField] private int i_enemiesPerWave;
+    [SerializeField] private Vector2 v_secondsBetweenWave;
+    [SerializeField] private Vector2 v_enemiesPerWave;
     [SerializeField] private bool b_spawnWaveAtStart;
     private int i_numberOfEnemies;
     [SerializeField] private int i_maxNumberOfEnemies;
@@ -35,32 +36,39 @@ public class EnemySpawner : MonoBehaviourPunCallbacks
         if (b_spawnWaveAtStart)
             SpawnEnemyWave();
 
-    }
 
-    private void Update()
-    {
         if (!PhotonNetwork.IsMasterClient)
-            return;
-
-        SpawnEnemyWave();
-
+            Invoke("SpawnEnemyWave", Random.Range(v_secondsBetweenWave.x, v_secondsBetweenWave.y));
     }
+
 
     private void SpawnEnemyWave()
     {
-        spawnCount += Time.deltaTime;
+        float _f_max = 0;
+        List<float> _fL = new List<float>();
 
-        if (spawnCount >= i_secondsBetweenWave)
+        for (int i = 0; i < fA_enemyPerWaveWeighting.Length; i++)
+            _f_max += fA_enemyPerWaveWeighting[i];
+
+
+        if (i_numberOfEnemies < i_maxNumberOfEnemies)
         {
-            spawnCount = 0;
-
-            if (i_numberOfEnemies < i_maxNumberOfEnemies)
+            for (int i = 0; i < fA_enemyPerWaveWeighting.Length; i++)
             {
-                for (int i = 0; i < i_enemiesPerWave; i++)
-                    SpawnEnemy(goA_enemiesToSpawnDuringAWave[0], tA_spawnPoints[Random.Range(0, tA_spawnPoints.Length)].position + new Vector3(-3 + (Random.value * 6), 0, -3 + (Random.value * 6)));
+                _fL.Add(_f_max);
+                _f_max += fA_enemyPerWaveWeighting[i];
             }
+            _fL.Add(_f_max * 2);
         }
 
+        for (int i = 0; i < _fL.Count; i++)
+        {
+            float rando = Random.Range(0, _f_max);
+            if (rando >= _fL[i] && rando < _fL[i + 1])
+                SpawnEnemy(goA_enemiesToSpawnDuringAWave[i], tA_spawnPoints[Random.Range(0, tA_spawnPoints.Length)].position + new Vector3(-3 + (Random.value * 6), 0, -3 + (Random.value * 6)));
+        }
+
+        Invoke("SpawnEnemyWave", Random.Range(v_secondsBetweenWave.x, v_secondsBetweenWave.y));
     }
 
     private void SpawnEnemy(GameObject toSpawn, Vector3 spawnPos)
