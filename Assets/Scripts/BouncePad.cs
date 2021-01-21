4983359f55776c1dd9569316628589fa99a71397
Nick-Furniss <44497325+Nick-Furniss@users.noreply.github.com>
2021-01-21 11:27:55 +0000

    Bounce pad where the stairs were

---
+++ Assets/Scripts/BouncePad.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BouncePad : MonoBehaviour
+{
+
+    [SerializeField] private LayerMask lm_layersToBounce;
+    [SerializeField] private float f_bounceForce;
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+            Jump();
+    }
+
+    public void Jump()
+    {
+        Collider[] _cA = Physics.OverlapSphere(transform.position, 1, lm_layersToBounce);
+
+        for (int i = 0; i < _cA.Length; i++)
+        {
+            _cA[i].GetComponent<Rigidbody>().AddForce(transform.up * f_bounceForce, ForceMode.Impulse);
+        }
+    }
+
+
+
+}
