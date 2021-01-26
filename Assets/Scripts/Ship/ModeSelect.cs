2a5e1a63b2f8bd3991c0ff7dea5683ade6347411
Nick-Furniss <44497325+Nick-Furniss@users.noreply.github.com>
2021-01-26 10:57:56 +0000

    ModeSelector

--- Assets/Scripts/Ship/ModeSelect.cs
+++ Assets/Scripts/Ship/ModeSelect.cs
@@ -7,18 +7,19 @@ public class ModeSelect : MonoBehaviour, IInteractible
 
     private int i_currentIndex;
     [SerializeField] private LoadIntoLevel lil_teleportButton;
-    [SerializeField] private GameObject go_highLight;
-    [Space, SerializeField] private Vector3[] vA_highlightPositions = new Vector3[3];
+    [Space, SerializeField] private GameObject[] goA_highlightPositions = new GameObject[3];
     [Space, SerializeField] private string[] sA_sceneNames = new string[3];
 
     public void Interacted()
     {
+        goA_highlightPositions[i_currentIndex].SetActive(false);
+
         i_currentIndex++;
 
-        if (i_currentIndex >= vA_highlightPositions.Length)
+        if (i_currentIndex >= goA_highlightPositions.Length)
             i_currentIndex = 0;
 
-        go_highLight.transform.localPosition = vA_highlightPositions[i_currentIndex];
+        goA_highlightPositions[i_currentIndex].SetActive(true);
         lil_teleportButton.levelToLoad = sA_sceneNames[i_currentIndex];
     }
 
