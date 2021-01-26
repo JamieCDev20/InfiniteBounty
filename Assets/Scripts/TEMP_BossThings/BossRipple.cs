be8543933e4cf249557fe66afc9627160c924f19
Nick-Furniss <44497325+Nick-Furniss@users.noreply.github.com>
2021-01-26 16:44:55 +0000

    Boss fixes

--- Assets/Scripts/TEMP_BossThings/BossRipple.cs
+++ Assets/Scripts/TEMP_BossThings/BossRipple.cs
@@ -8,8 +8,13 @@ public class BossRipple : BossProjectile
     [Header("Ripple")]
     [SerializeField] private float f_growthRate;
 
+    private void Start()
+    {
+        transform.position -= Vector3.up * 7;
+    }
+
     void Update()
     {
-        transform.localScale += f_growthRate * Time.deltaTime * (Vector3.one - (Vector3.up * 0.5f));
+        transform.localScale += f_growthRate * Time.deltaTime * (Vector3.one));
     }
 }
---
+++ Assets/Scripts/TEMP_BossThings/LilyPad.cs
@@ -0,0 +1,45 @@
+﻿using Photon.Pun;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LilyPad : MonoBehaviour, IHitable
+{
+    private int i_currentHealth = 50;
+    private PhotonView view;
+    private bool b_isHost;
+
+    private void Start()
+    {
+        view = GetComponent<PhotonView>();
+        b_isHost = PhotonNetwork.IsMasterClient;
+    }
+
+    public void Die()
+    {
+        gameObject.SetActive(false);
+    }
+
+    public bool IsDead()
+    {
+        return false;
+    }
+
+    public void TakeDamage(int damage, bool activatesThunder)
+    {
+        if (b_isHost)
+            view.RPC("ActualTakeDamage", RpcTarget.All, damage);
+    }
+    [PunRPC]
+    public void ActualTakeDamage(int damage)
+    {
+        i_currentHealth -= damage;
+        if (i_currentHealth < 0)
+            Die();
+    }
+
+    public void TakeDamage(int damage, bool activatesThunder, float delay)
+    {
+
+    }
+}
