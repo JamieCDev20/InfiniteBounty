7b3aa0d8597a85300538209f60bf30433bc51a5b
Nick-Furniss <44497325+Nick-Furniss@users.noreply.github.com>
2021-01-28 10:47:20 +0000

    First pass bubble shield

---
+++ Assets/Scripts/Tools/Mobility/BubbleShield.cs
@@ -0,0 +1,78 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BubbleShield : MobilityTool
+{
+
+    [Header("Bubble Shield Things")]
+    [SerializeField] private GameObject go_shieldPrefab;
+    private List<GameObject> goL_shieldPool = new List<GameObject>();
+    [SerializeField] private Transform t_firePoint;
+    [SerializeField] private float f_orbLifeTime;
+    [SerializeField] private float f_shootForce;
+    private float f_useTimer;
+    [SerializeField] private GameObject go_growth;
+    private bool b_isOnCoolDown;
+    [SerializeField] private Material m_onCooldownMaterial;
+    [SerializeField] private Material m_offCooldownMaterial;
+
+    private void Awake()
+    {
+        f_useTimer = f_timeBetweenUsage;
+        for (int i = 0; i < 2; i++)
+        {
+            GameObject _go = Instantiate(go_shieldPrefab, transform);
+            _go.SetActive(false);
+            goL_shieldPool.Add(_go);
+        }
+    }
+
+    private void Update()
+    {
+        if (f_useTimer <= f_timeBetweenUsage)
+            go_growth.transform.localScale = Vector3.one * (float)(f_useTimer / f_timeBetweenUsage);
+        else if (b_isOnCoolDown)
+        {
+            b_isOnCoolDown = false;
+            go_growth.GetComponent<MeshRenderer>().material = m_offCooldownMaterial;
+        }
+
+        f_useTimer += Time.deltaTime;
+    }
+
+    public override void Use(Vector3 _v_forwards)
+    {
+        ShootOrb(_v_forwards);
+    }
+
+    public override void Use() { }
+    public override void NetUse(Vector3 _v_forwards) { }
+
+    private void ShootOrb(Vector3 _v_forwards)
+    {
+        if (f_useTimer > f_timeBetweenUsage)
+        {
+            GameObject _go = goL_shieldPool[0];
+            goL_shieldPool.RemoveAt(0);
+            _go.transform.parent = null;
+            _go.transform.position = t_firePoint.position;
+            _go.SetActive(true);
+            _go.GetComponent<Rigidbody>().AddForce(_v_forwards * f_shootForce);
+
+            StartCoroutine(ReturnOrbToPool(_go));
+            f_useTimer = 0;
+            b_isOnCoolDown = true;
+            go_growth.GetComponent<MeshRenderer>().material = m_onCooldownMaterial;
+        }
+    }
+
+    private IEnumerator ReturnOrbToPool(GameObject _go_orb)
+    {
+        yield return new WaitForSeconds(f_orbLifeTime);
+        _go_orb.SetActive(false);
+        goL_shieldPool.Add(_go_orb);
+    }
+
+
+}
