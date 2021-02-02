c9178a753e8774192f8257d10c051b4894e199e8
Nick-Furniss <44497325+Nick-Furniss@users.noreply.github.com>
2021-02-02 09:45:58 +0000

    Maybe fixed boss

---
+++ Assets/Scripts/TEMP_BossThings/BossTarget.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BossTarget : MonoBehaviour
+{
+    [SerializeField] private LayerMask lm_playerLayer;
+
+    void Start()
+    {
+        Collider[] _cA = Physics.OverlapSphere(transform.position, 5, lm_playerLayer);
+
+        for (int i = 0; i < _cA.Length; i++)
+            if (_cA[i].CompareTag("Player"))
+            {
+                transform.parent = _cA[i].transform;
+                transform.localPosition = Vector3.up;
+                return;
+            }
+
+        Destroy(gameObject);
+    }
+}
