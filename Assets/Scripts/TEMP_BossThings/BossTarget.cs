3b208862da79a55abf8f80816f7acd18bf653a91
Nick-Furniss <44497325+Nick-Furniss@users.noreply.github.com>
2021-02-04 12:00:28 +0000

    Update BossTarget.cs

--- Assets/Scripts/TEMP_BossThings/BossTarget.cs
+++ Assets/Scripts/TEMP_BossThings/BossTarget.cs
@@ -4,13 +4,13 @@ using UnityEngine;
 
 public class BossTarget : MonoBehaviour
 {
-    [SerializeField] private LayerMask lm_playerLayer;
-    [SerializeField] private float f_delay;
     [SerializeField] private TEMP_Boss tb_boss;
 
-    IEnumerator Start()
+
+    private IEnumerator Start()
     {
-        yield return new WaitForEndOfFrame();
+        for (int i = 0; i < 4; i++)
+            yield return new WaitForEndOfFrame();
 
         Collider[] _cA = Physics.OverlapSphere(transform.position, 5);
 
