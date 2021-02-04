476bcceee22051e41fe413b99ea23c43d326b283
johnarthurmeakin <johnnyjam23@hotmail.com>
2021-02-04 10:47:14 +0000

    Added the Footstepplayer script and ground chaeck GO to player prefab

---
+++ Assets/FootstepAudioPlayer.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum Surface
+{
+    ship, planet
+}
+
+public class FootstepAudioPlayer : MonoBehaviour
+{
+    [SerializeField] private AudioClip ac_footstepShip;
+    [SerializeField] private AudioClip ac_footstepPlanet;
+    [SerializeField] private AudioSource as_feetAudio;
+
+    private Surface e_surface;
+
+    public void ChangeSurfaceEnum(Surface _surface)
+    {
+        e_surface = _surface;
+    }
+
+    public void PlayFootstepSound()
+    {
+        switch (e_surface)
+        {
+            case Surface.ship:
+                as_feetAudio.PlayOneShot(ac_footstepShip);
+                break;
+            case Surface.planet:
+                as_feetAudio.PlayOneShot(ac_footstepPlanet);
+                break;
+            default:
+                break;
+        }
+    }
+}
