415f8fb9c0072475a854ad66beb0a21302d27bc4
Jamboc731 <jamiec.covellj@gmail.com>
2021-02-04 14:10:54 +0000

    Fixed walking bug

--- Assets/Scripts/Player/PlayerAnimator.cs
+++ Assets/Scripts/Player/PlayerAnimator.cs
@@ -124,6 +124,8 @@ public class PlayerAnimator : MonoBehaviourPun
             Vector3 vec = Vector3.Scale(rb.velocity, Vector3.one - Vector3.up);
             anim.SetFloat("X", Mathf.Lerp(anim.GetFloat("X"), pm_mover.v_movementVector.x * (pm_mover.b_sprintHold ? 2 : 1), Time.deltaTime * 4));
             anim.SetFloat("Y", Mathf.Lerp(anim.GetFloat("Y"), pm_mover.v_movementVector.z * (pm_mover.b_sprintHold ? 2 : 1), Time.deltaTime * 4));
+            if (rb.velocity.sqrMagnitude > 0.1f)
+                return;
             if (Mathf.Abs(anim.GetFloat("Y")) < 0.05f)
                 anim.SetFloat("Y", 0);
             if (Mathf.Abs(anim.GetFloat("X")) < 0.05f)
