30f6c62c2d16b749d8027f953c5a4ff72e770b10
Nick-Furniss <44497325+Nick-Furniss@users.noreply.github.com>
2021-02-08 10:13:51 +0000

    ag

--- Assets/Scripts/ZZZ_TEstingNetwrok.cs
+++ Assets/Scripts/ZZZ_TEstingNetwrok.cs
@@ -1,4 +1,5 @@
 ﻿using Photon.Pun;
+using Photon.Realtime;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -12,10 +13,14 @@ public class ZZZ_TEstingNetwrok : MonoBehaviourPunCallbacks
     {
         x = this;
         PhotonNetwork.ConnectUsingSettings();
-
     }
 
-    public override void OnConnected()
+    public override void OnConnectedToMaster()
+    {
+        PhotonNetwork.JoinOrCreateRoom("Do not come in here", new RoomOptions { MaxPlayers = 2 }, TypedLobby.Default);
+    }
+
+    public override void OnJoinedRoom()
     {
         PhotonNetwork.Instantiate(s_playerPath, transform.position, Quaternion.identity);
     }
