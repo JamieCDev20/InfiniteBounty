ff5e51d1a062ba4479bf781b5d58e2cb06c3effa
Jamboc731 <jamiec.covellj@gmail.com>
2021-02-11 11:16:30 +0000

    Update Lobby.cs

--- Assets/Scripts/Testing/LobbyScripts/Lobby.cs
+++ Assets/Scripts/Testing/LobbyScripts/Lobby.cs
@@ -63,14 +63,14 @@ public class Lobby : MonoBehaviourPunCallbacks
     public override void OnConnectedToMaster()
     {
         Debug.Log("Connected");
-        if (HostButton != null)
         //PhotonNetwork.JoinLobby();
     }
 
     public override void OnJoinedLobby()
     {
         Debug.Log("joined lobby");
-        HostButton.interactable = true;
+        if (HostButton != null)
+            HostButton.interactable = true;
     }
 
     public override void OnJoinedRoom()
