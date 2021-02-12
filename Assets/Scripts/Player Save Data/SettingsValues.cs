0ae1cdae58828bf7a7af1eddd51c4116a8015016
Byron Crowhurst <57679765+ByPossum@users.noreply.github.com>
2021-02-12 14:33:35 +0000

    Settings data is a little broken. Trying to fix.

---
+++ Assets/Scripts/Player Save Data/SettingsValues.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SettingsValues
+{
+    public bool invertY;
+    public float[] A_settingFloats;
+    public SettingsValues()
+    {
+
+    }
+    public SettingsValues(bool _y, float[] _A_settings)
+    {
+        invertY = _y;
+        A_settingFloats = _A_settings;
+    }
+}
