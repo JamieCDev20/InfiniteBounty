2b000e943f0803e2c0f0b18951e094b21e61b031
Jamboc731 <jamiec.covellj@gmail.com>
2021-02-18 11:49:20 +0000

    Update OfflinePlayer.cs

--- Assets/Scripts/Bluetack/OfflinePlayer.cs
+++ Assets/Scripts/Bluetack/OfflinePlayer.cs
@@ -6,10 +6,28 @@ public class OfflinePlayer : MonoBehaviour
 {
     [SerializeField] private CameraController c_control;
     [SerializeField] private PlayerInputManager pim;
+    [SerializeField] private OfflineToolLoad tool;
+
+    private ToolHandler th;
 
     private void Awake()
     {
+        th = GetComponent<ToolHandler>();
         pim.SetCamera(c_control);
+        Invoke(nameof(GiveTool), 0.5f);
     }
 
+    private void GiveTool()
+    {
+        th.SwapTool(ToolSlot.moblility, (int)tool);
+    }
+
+}
+
+public enum OfflineToolLoad
+{
+    Teleporter,
+    Jetpack,
+    Lunchbox,
+    BubbleShield
 }
