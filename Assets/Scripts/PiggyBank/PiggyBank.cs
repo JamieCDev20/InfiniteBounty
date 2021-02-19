ae43daea4ab965c2da740cc7de3d1b36a9492b40
Byron Crowhurst <57679765+ByPossum@users.noreply.github.com>
2021-02-19 12:07:32 +0000

    Piggy Bank script created

---
+++ Assets/Scripts/PiggyBank/PiggyBank.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PiggyBank : MonoBehaviour, IInteractible
+{
+    [SerializeField] private int i_targetAmount;
+    [SerializeField] private int i_inputAmount;
+    [SerializeField] private int i_storedAmount;
+    [SerializeField] GameObject go_portal;
+    public void Interacted()
+    {
+        
+    }
+
+    public void Interacted(Transform interactor)
+    {
+        if(interactor?.GetComponent<NugManager>().Nugs >= i_inputAmount)
+        {
+            interactor.GetComponent<NugManager>().CollectNugs(-i_inputAmount, false);
+            i_storedAmount += i_inputAmount;
+            if(i_storedAmount >= i_targetAmount)
+            {
+                EnablePortal();
+            }
+        }
+    }
+
+    public void SetInputAmount(int _input)
+    {
+        i_inputAmount = _input;
+    }
+
+    private void EnablePortal()
+    {
+        go_portal.SetActive(true);
+        gameObject.SetActive(false);
+    }
+}
