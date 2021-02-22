b726bf36abb6efa934ed01ad2fca811d45d22c7d
Jamboc731 <jamiec.covellj@gmail.com>
2021-02-22 11:07:42 +0000

    Nugg magnet

---
+++ Assets/Scripts/Player/NugMagnet.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NugMagnet : MonoBehaviour
+{
+
+    private void OnTriggerStay(Collider other)
+    {
+        if (other.gameObject.layer == 9)
+        {
+            other.gameObject.GetComponent<Rigidbody>().velocity = (transform.position - other.transform.position).normalized * 10;
+        }
+    }
+
+}
