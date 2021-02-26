7b872bf0aeb859d225ddce37bfda0721efd51eee
Nick-Furniss <44497325+Nick-Furniss@users.noreply.github.com>
2021-02-26 13:56:34 +0000

    Teleporter rework?

---
+++ Assets/Scripts/Tools/Mobility/Teleportal.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Teleportal : MonoBehaviour
+{
+    private bool b_isOpen;
+    [SerializeField] private Teleportal tp_otherPortal;
+    private List<Rigidbody> rbL_recentlyTeleported = new List<Rigidbody>();
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (b_isOpen)
+        {
+            Rigidbody _rb = other.GetComponent<Rigidbody>();
+            if (_rb != null)
+            {
+                if (!rbL_recentlyTeleported.Contains(_rb))
+                {
+                    rbL_recentlyTeleported.Add(_rb);
+                    StartCoroutine(RemoveFromPool(_rb));
+
+                    other.transform.position = tp_otherPortal.transform.position;
+                    tp_otherPortal.CloseForCooldown(_rb);
+                }
+            }
+        }
+    }
+
+    private IEnumerator RemoveFromPool(Rigidbody _rb)
+    {
+        yield return new WaitForSeconds(0.5f);
+        rbL_recentlyTeleported.Remove(_rb);
+    }
+
+    internal void CloseForCooldown(Rigidbody _rb)
+    {
+        rbL_recentlyTeleported.Add(_rb);
+        StartCoroutine(RemoveFromPool(_rb));
+    }
+
+    internal void Setup(float _f_lifeSpan)
+    {
+        StartCoroutine(ClosePortal(_f_lifeSpan));
+        GetComponentInChildren<ParticleSystem>().Play();
+    }
+
+    private IEnumerator ClosePortal(float _f_lifeSpan)
+    {
+        yield return new WaitForSeconds(1);
+        b_isOpen = true;
+
+        yield return new WaitForSeconds(_f_lifeSpan);
+        GetComponentInChildren<ParticleSystem>().Stop();
+        b_isOpen = false;
+
+        yield return new WaitForSeconds(5);
+        gameObject.SetActive(false);
+    }
+
+
+}
--- Assets/Scripts/Tools/Mobility/Teleporter.cs
+++ Assets/Scripts/Tools/Mobility/Teleporter.cs
@@ -1,4 +1,5 @@
 ﻿
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -11,12 +12,15 @@ public class Teleporter : MobilityTool
     private float f_coolDown;
     [SerializeField] private float f_teleportDelay;
     [Space, SerializeField] private GameObject go_chargeEffects;
-    [SerializeField] private GameObject go_leavingEffect;
-    [SerializeField] private GameObject go_arrivalEffect;
     [SerializeField] private Material redMat;
     [SerializeField] private Material greenMat;
     [SerializeField] private Renderer lightRenderer;
     private AudioSource as_source;
+
+    [Header("Portals")]
+    [SerializeField] private GameObject go_startPortal;
+    [SerializeField] private GameObject go_endPortal;
+    [SerializeField] private float f_portalLifeSpawn;
 
     private void Start()
     {
@@ -36,40 +40,50 @@ public class Teleporter : MobilityTool
     {
         if (b_isActive)
         {
-            go_leavingEffect.transform.parent = transform;
-            go_leavingEffect.transform.localPosition = Vector3.zero;
-            go_leavingEffect.SetActive(false);
-            go_leavingEffect.SetActive(true);
-
-            StartCoroutine(DoTheTeleport(_v_lookDirection));
-
-            PlayAudio(ac_activationSound);
+            DoTheTeleport(_v_lookDirection);
         }
     }
 
-    private IEnumerator DoTheTeleport(Vector3 _v_lookDirection)
+    private void DoTheTeleport(Vector3 _v_lookDirection)
     {
         if (b_isActive)
         {
-            lightRenderer.material = redMat;
-            yield return new WaitForSeconds(f_teleportDelay);
-            go_leavingEffect.transform.parent = null;
-            RaycastHit _hit;
-            if (Physics.Raycast(transform.position, _v_lookDirection, out _hit, f_teleportDistance, gameObject.layer))
-            {
-                transform.root.position = _hit.point;
-            }
-            else
-                transform.root.position += _v_lookDirection * f_teleportDistance;
+            RaycastHit[] _hits;
+            _hits = Physics.RaycastAll(transform.position, _v_lookDirection, Mathf.Infinity, LayerMask.NameToLayer("Ignore Raycasts"), QueryTriggerInteraction.Ignore);
 
-            BeginCooldown();
-
-            go_arrivalEffect.transform.parent = null;
-            go_arrivalEffect.transform.position = transform.position;
-            go_arrivalEffect.SetActive(false);
-            go_arrivalEffect.SetActive(true);
+            for (int i = 0; i < _hits.Length; i++)
+                if (_hits[i].transform.CompareTag("Player"))
+                {
+                    OpenPortalAtPoint(_hits[i], _v_lookDirection);
+                    BeginCooldown();
+                    return;
+                }
+            if (_hits.Length > 0)
+                if (Vector3.Distance(transform.position, _hits[0].point) <= f_teleportDistance)
+                {
+                    OpenPortalAtPoint(_hits[0], _v_lookDirection);
+                    PlayAudio(ac_activationSound);
+                    BeginCooldown();
+                }
         }
     }
+
+    private void OpenPortalAtPoint(RaycastHit hit, Vector3 _v_lookDirection)
+    {
+        go_startPortal.transform.position = transform.position + _v_lookDirection * 5;
+        go_startPortal.SetActive(true);
+        go_startPortal.transform.parent = null;
+        DontDestroyOnLoad(go_startPortal);
+        go_startPortal.GetComponent<Teleportal>().Setup(f_portalLifeSpawn);
+
+        go_endPortal.transform.position = hit.point - _v_lookDirection * 3;
+        go_endPortal.SetActive(true);
+        go_endPortal.transform.parent = null;
+        DontDestroyOnLoad(go_endPortal);
+        go_endPortal.GetComponent<Teleportal>().Setup(f_portalLifeSpawn);
+
+    }
+
 
     private void ComeOffCooldown()
     {
