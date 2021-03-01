bf49df8a0802865919f5b107dc5044b0ca23a4ea
Jamboc731 <jamiec.covellj@gmail.com>
2021-03-01 17:02:57 +0000

    Update NugGO.cs

--- Assets/Scripts/Nugs/NugGO.cs
+++ Assets/Scripts/Nugs/NugGO.cs
@@ -80,7 +80,7 @@ public class NugGO : SubjectBase, IPoolable, ISuckable, IHitable
             AudioSource.PlayClipAtPoint(ac_pickupSound, transform.position);
         if(rb != null)
             rb.velocity = Vector3.zero;
-        eO_elem.ResetElements();
+        eO_elem?.ResetElements();
         PoolManager.x.ReturnObjectToPool(gameObject);
     }
 
