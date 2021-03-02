69ac7afe6e17a0150d245e1142c29a21980d9bac
Jamboc731 <jamiec.covellj@gmail.com>
2021-03-02 15:01:55 +0000

    Update ScoreboardManager.cs

--- Assets/Scripts/Managers/ScoreboardManager.cs
+++ Assets/Scripts/Managers/ScoreboardManager.cs
@@ -39,7 +39,12 @@ public class ScoreboardManager : MonoBehaviour
         int playerTotal;
         for (int i = 0; i < PhotonNetwork.CurrentRoom.PlayerCount; i++)
         {
+            try
+            {
             so_playerScoreObjects[i].nameText.text = PhotonNetwork.CurrentRoom.Players[i].NickName;
+
+            }
+            catch { }
             so_playerScoreObjects[i].gooText.text = values[i][0].ToString();
             so_playerScoreObjects[i].hydroText.text = values[i][1].ToString();
             so_playerScoreObjects[i].tastyText.text = values[i][2].ToString();
