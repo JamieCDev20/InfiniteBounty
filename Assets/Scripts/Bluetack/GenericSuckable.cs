eee62f167b3149401b6965d86b9079d7b442a05a
Nick-Furniss <44497325+Nick-Furniss@users.noreply.github.com>
2021-03-12 09:59:59 +0000

    Generic Suckable & floor signs

---
+++ Assets/Scripts/Bluetack/GenericSuckable.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GenericSuckable : MonoBehaviour, ISuckable
+{
+    private Rigidbody rb;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    public Rigidbody GetRigidbody()
+    {
+        return rb;
+    }
+}
