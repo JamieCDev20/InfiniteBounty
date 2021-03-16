4e24684e3b95aa42294117df3fa3e68c98f0d5aa
Jamboc731 <jamiec.covellj@gmail.com>
2021-03-16 15:27:34 +0000

    this will workk so well

---
+++ Assets/Scripts/Lodes/TheOneTrueSpawner.cs
@@ -0,0 +1,33 @@
+﻿using Photon.Pun;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TheOneTrueSpawner : MonoBehaviourPun
+{
+
+    [SerializeField] private LodeSpawnZone[] lszA_zones;
+
+    private void Start()
+    {
+        photonView.RPC(nameof(SyncSeed), RpcTarget.All, 1);
+    }
+
+    [PunRPC]
+    private void SyncSeed(int seed)
+    {
+        Random.InitState(1);
+        StartCoroutine(DoASpawn());
+
+    }
+
+    IEnumerator DoASpawn()
+    {
+        for (int i = 0; i < lszA_zones.Length; i++)
+        {
+            lszA_zones[i].SpawnLode();
+            yield return new WaitForEndOfFrame();
+        }
+    }
+
+}
