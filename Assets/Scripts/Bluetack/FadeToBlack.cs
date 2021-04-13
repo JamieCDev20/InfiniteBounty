54156d83ee1f76c1df3e56f54b13969b48d47610
Nick-Furniss <44497325+Nick-Furniss@users.noreply.github.com>
2021-04-13 11:44:36 +0100

    Loading fixes

--- Assets/Scripts/Bluetack/FadeToBlack.cs
+++ Assets/Scripts/Bluetack/FadeToBlack.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 
 public class FadeToBlack : MonoBehaviour
@@ -9,21 +10,39 @@ public class FadeToBlack : MonoBehaviour
     [SerializeField] private Image i_coverImage;
 
 
-    private IEnumerator Start()
+    private void Start()
     {
-        x = this;
-        yield return new WaitForSeconds(1);
-        HideCover();
+        if (x == null)
+        {
+            x = this;
+            DontDestroyOnLoad(gameObject);
+        }
+        else
+            Destroy(gameObject);
+
+        SceneManager.sceneLoaded += DefaultHideCover;
+        HideCover(1);
     }
 
-    public void HideCover()
+    private void DefaultHideCover(Scene s, LoadSceneMode m)
     {
-        i_coverImage.CrossFadeAlpha(0, 0.7f, true);
+        HideCover(1);
     }
 
-    public void ShowCover()
+    public void HideCover(float delay)
     {
-        i_coverImage.CrossFadeAlpha(1, 0.7f, true);
+        StartCoroutine(IChangeCover(delay, 0));
+    }
+
+    public void ShowCover(float delay)
+    {
+        StartCoroutine(IChangeCover(delay, 1));
+    }
+
+    public IEnumerator IChangeCover(float delay, float _f_targetAlpha)
+    {
+        yield return new WaitForSeconds(delay);
+        i_coverImage.CrossFadeAlpha(_f_targetAlpha, 0.7f, true);
     }
 
 }
--- Assets/Scripts/Bluetack/LoadingScreenManager.cs
+++ Assets/Scripts/Bluetack/LoadingScreenManager.cs
@@ -68,7 +68,7 @@ public class LoadingScreenManager : MonoBehaviourPun
     [PunRPC]
     public void ShowLoadingScreen()
     {
-        FadeToBlack.x.ShowCover();
+        FadeToBlack.x.ShowCover(0);
     }
 
     internal IEnumerator BeginLoadingSceneAsync()
