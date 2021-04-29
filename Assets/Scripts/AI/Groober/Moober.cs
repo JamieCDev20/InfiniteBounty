287632acdbf5132ee8b463c73e53a588d3a39748
Jamboc731 <jamiec.covellj@gmail.com>
2021-04-29 10:32:52 +0100

    Create Moober.cs

---
+++ Assets/Scripts/AI/Groober/Moober.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Moober : MoverBase
+{
+
+    public override void Move(Vector3 _dir)
+    {
+        base.Move(_dir);
+    }
+
+}
