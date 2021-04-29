af6e60ff484aed15bad20080736178257612cb93
Jamboc731 <jamiec.covellj@gmail.com>
2021-04-29 11:54:18 +0100

    Update Moober.cs

--- Assets/Scripts/AI/Groober/Moober.cs
+++ Assets/Scripts/AI/Groober/Moober.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class Moober : MoverBase
 {
-
+    [SerializeField] private LayerMask jumpMask;
     public override void Move(Vector3 _dir)
     {
 
@@ -12,8 +12,8 @@ public class Moober : MoverBase
         rb.AddForce(Vector3.ProjectOnPlane(_dir, Vector3.up).normalized * f_moveSpeed * (b_grounded ? 1 : f_airControlMultiplier), ForceMode.Impulse);
         if (rb.velocity.magnitude > 0.1f)
             transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(Vector3.Scale(rb.velocity, Vector3.one - Vector3.up), Vector3.up), 0.2f);
-        LayerMask mask = ~LayerMask.NameToLayer("Player");
-        if (Physics.Raycast(transform.position + (Vector3.up * 0.5f), transform.forward, 1.5f, mask))
+
+        if (Physics.Raycast(transform.position + (Vector3.up * 0.5f), transform.forward, 1.5f, jumpMask))
         {
             if(Time.realtimeSinceStartup - lastJumped > 1.5f)
             {
