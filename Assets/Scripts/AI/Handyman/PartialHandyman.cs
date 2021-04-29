45a5ac0a04d5317ed5ec88a571cc97ecd9dce0bf
Nick-Furniss <44497325+Nick-Furniss@users.noreply.github.com>
2021-04-29 14:17:28 +0100

    Update PartialHandyman.cs

--- Assets/Scripts/AI/Handyman/PartialHandyman.cs
+++ Assets/Scripts/AI/Handyman/PartialHandyman.cs
@@ -5,6 +5,72 @@ using UnityEngine;
 public partial class HandymanAI : AIBase
 {
 
+    [Header("Punch Attack")]
+    [SerializeField] private float f_punchRange;
+    private bool b_shouldBeThrowing;
 
+    [Header("Throwing Stats")]
+    [SerializeField] private float f_pickupRange;
+    private GameObject go_nearestThrowable;
+    private bool b_hasThrowable;
+    [SerializeField] private float f_minThrowDistance;
+
+
+    #region Queries
+
+    private bool IsInPunchRangeQuery()
+    {
+        return Vector3.Distance(transform.position, t_target.position) <= f_punchRange;
+    }
+
+    private bool IsInPickUpRangeQuery()
+    {
+        return Vector3.Distance(transform.position, go_nearestThrowable.transform.position) <= f_pickupRange;
+    }
+
+    private bool HasThrowableObjectQuery()
+    {
+        return b_hasThrowable;
+    }
+
+    #endregion
+
+    #region Actions
+
+    private GameObject GetClosestTaggedObjectAction(string _s_tag)
+    {
+        float _f_distance = 1000000000;
+        GameObject go_object = null;
+
+        foreach (GameObject item in TagManager.x.GetTagSet(_s_tag))
+        {
+            float _f_distanceCheck = Vector3.SqrMagnitude(item.transform.position - transform.position);
+            if (_f_distanceCheck < _f_distance)
+            {
+                go_object = item;
+                _f_distance = _f_distanceCheck;
+            }
+        }
+
+        return go_object;
+    }
+
+    private void GetClosestPlayerAction()
+    {
+        t_target = GetClosestTaggedObjectAction("Player").transform;
+    }
+
+    private void GetClosestThrowableObjectAction()
+    {
+        go_nearestThrowable = GetClosestTaggedObjectAction("Throwable");
+    }
+
+    private void CheckShouldBeThrowing()
+    {
+        b_shouldBeThrowing = Vector3.Distance(transform.position, t_target.position) >= f_minThrowDistance;
+    }
+
+
+    #endregion
 
 }
