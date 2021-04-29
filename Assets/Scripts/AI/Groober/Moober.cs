f259fd7d210ac9b1ac9373b513f0ed5f52daf04c
Jamboc731 <jamiec.covellj@gmail.com>
2021-04-29 11:47:17 +0100

    better jumping

--- Assets/Scripts/AI/Groober/Moober.cs
+++ Assets/Scripts/AI/Groober/Moober.cs
@@ -12,10 +12,15 @@ public class Moober : MoverBase
         rb.AddForce(Vector3.ProjectOnPlane(_dir, Vector3.up).normalized * f_moveSpeed * (b_grounded ? 1 : f_airControlMultiplier), ForceMode.Impulse);
         if (rb.velocity.magnitude > 0.1f)
             transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(Vector3.Scale(rb.velocity, Vector3.one - Vector3.up), Vector3.up), 0.3f);
-
-        if (Physics.Raycast(transform.position + (Vector3.up * 0.5f), transform.forward, 0.5f))
+        LayerMask mask = ~LayerMask.NameToLayer("Player");
+        if (Physics.Raycast(transform.position + (Vector3.up * 0.5f), transform.forward, 1.5f, mask))
         {
-            rb.AddForce(Vector3.up * f_jumpForce, ForceMode.Impulse);
+            if(Time.realtimeSinceStartup - lastJumped > 1.5f)
+            {
+                rb.AddForce(Vector3.up * f_jumpForce, ForceMode.Impulse);
+                lastJumped = Time.realtimeSinceStartup;
+
+            }
         }
 
     }
