4b2d2191a65f1009ab6ba7e51dd4a717dcc56f5b
Jamboc731 <jamiec.covellj@gmail.com>
2021-05-04 13:50:34 +0100

    handy updates and flying starts

--- Assets/Scripts/AI/FlyingBoi/FlyingAI.cs
+++ Assets/Scripts/AI/FlyingBoi/FlyingAI.cs
@@ -3,7 +3,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class FlyingAI : AIBase
+public partial class FlyingAI : AIBase
 {
 
     //float f_summoningSickness = 1;
