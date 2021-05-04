47515b71adf3d312cb233e21276f4c1f751a4870
Jamboc731 <jamiec.covellj@gmail.com>
2021-05-04 09:39:18 +0100

    once more

---
+++ Assets/Scripts/TrailerController.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+public class TrailerPlayerController : MonoBehaviour
+{
+    [SerializeField] private Animator anim;
+    private void Update()
+    {
+        Vector3 movement = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical")) * (Input.GetButton("Sprint") ? 2 : 1);
+        anim.SetFloat("X", movement.x);
+        anim.SetFloat("Y", movement.z);
+        anim.SetBool("ShootingRight", Input.GetMouseButton(1));
+        anim.SetBool("ShootingLeft", Input.GetMouseButton(0));
+        if (Input.GetKeyDown(KeyCode.K))
+            anim.SetTrigger("GetKnockedDown");
+        anim.SetBool("GetRevived", Input.GetKey(KeyCode.R));
+    }
+}
