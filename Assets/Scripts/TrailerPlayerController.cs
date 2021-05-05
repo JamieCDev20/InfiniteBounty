e6af611e2f1b7c938b570bad91657ab6c5aae9a7
bartocgd <55786210+bartocgd@users.noreply.github.com>
2021-05-05 13:30:14 +0100

    mk n

--- Assets/Scripts/TrailerPlayerController.cs
+++ Assets/Scripts/TrailerPlayerController.cs
@@ -6,6 +6,12 @@ public class TrailerPlayerController : MonoBehaviour
     [SerializeField] private Animator anim;
     [SerializeField] private float TransitionSpeed = 0.2f;
     Vector3 movement;
+    public GameObject sputnik1;
+    public GameObject sputnik2;
+    public GameObject sputnik3;
+    public GameObject sputnik4;
+
+
     private void Update()
     {
         movement = Vector3.Lerp(movement, new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical")) * (Input.GetButton("Sprint") ? 2 : 1) * (Input.GetKey(KeyCode.LeftControl) ? 2 : 1), TransitionSpeed);
@@ -16,5 +22,15 @@ public class TrailerPlayerController : MonoBehaviour
         if (Input.GetKeyDown(KeyCode.K))
             anim.SetTrigger("GetKnockedDown");
         anim.SetBool("GetRevived", Input.GetKey(KeyCode.R));
+        if (Input.GetMouseButton(0))
+        {
+            sputnik1.SetActive(false);
+            sputnik2.SetActive(true);
+        }
+        if (Input.GetMouseButton(1))
+        {
+            sputnik3.SetActive(false);
+            sputnik4.SetActive(true);
+        }
     }
 }
