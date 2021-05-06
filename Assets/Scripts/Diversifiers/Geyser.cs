872a7e20119598a38b8dd0cc2a807050908ec96a
Nick-Furniss <44497325+Nick-Furniss@users.noreply.github.com>
2021-05-06 11:33:00 +0100

    Geyser SFX

--- Assets/Scripts/Diversifiers/Geyser.cs
+++ Assets/Scripts/Diversifiers/Geyser.cs
@@ -5,10 +5,18 @@ using UnityEngine;
 public class Geyser : MonoBehaviour
 {
     [SerializeField] private float f_firePower;
+    private AudioSource as_source;
+    [SerializeField] private AudioClip ac_jumpClip;
+
+    private void Start()
+    {
+        as_source = GetComponent<AudioSource>();
+    }
 
     private void OnTriggerEnter(Collider other)
     {
         Rigidbody _rb = other.GetComponent<Rigidbody>();
+        as_source.PlayOneShot(ac_jumpClip);
         if (_rb && !other.name.Contains("andyman"))
             _rb.velocity = transform.up * f_firePower;
     }
