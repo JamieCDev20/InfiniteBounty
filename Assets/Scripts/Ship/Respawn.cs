e87b389f21349a46628ee6a6264c19e9cacdb2a4
Jamboc731 <jamiec.covellj@gmail.com>
2021-05-11 12:26:13 +0100

    stuff

---
+++ Assets/Scripts/Ship/Respawn.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Respawn : MonoBehaviour
+{
+    private void Start()
+    {
+        foreach (PlayerHealth h in FindObjectsOfType<PlayerHealth>())
+        {
+            h.FullRespawn();
+        }
+    }
+}
