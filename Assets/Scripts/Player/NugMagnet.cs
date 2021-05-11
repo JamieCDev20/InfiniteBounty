39ccc0f8820937f64e1d7d6f8ce51fa3f0f8e6cc
Jamboc731 <jamiec.covellj@gmail.com>
2021-05-11 09:40:27 +0100

    new nugg magnet

--- Assets/Scripts/Player/NugMagnet.cs
+++ Assets/Scripts/Player/NugMagnet.cs
@@ -5,11 +5,13 @@ using UnityEngine;
 public class NugMagnet : MonoBehaviour
 {
 
+    [SerializeField] private float f_magnetStrength = 20f;
+
     private void OnTriggerStay(Collider other)
     {
         if (other.gameObject.layer == 9)
         {
-            other.gameObject.GetComponent<Rigidbody>().velocity = (transform.position - other.transform.position).normalized * 10;
+            other.gameObject.GetComponent<Rigidbody>().velocity = (transform.position - other.transform.position).normalized * f_magnetStrength;
         }
     }
 
