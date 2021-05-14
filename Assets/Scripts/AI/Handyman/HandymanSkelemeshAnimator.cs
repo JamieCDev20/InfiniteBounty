30bf1427d2dfd20790def01547ed444eb1fd4675
Jamboc731 <jamiec.covellj@gmail.com>
2021-05-14 11:00:50 +0100

    animation event functions set up

---
+++ Assets/Scripts/AI/Handyman/HandymanSkelemeshAnimator.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HandymanSkelemeshAnimator : MonoBehaviour
+{
+
+    [SerializeField] private AudioSource source;
+    [SerializeField] private AudioClip footClip;
+
+    public void PlayFootstep()
+    {
+        source.PlayOneShot(footClip);
+    }
+
+}
