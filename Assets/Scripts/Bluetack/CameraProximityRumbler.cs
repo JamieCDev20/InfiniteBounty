45aabe9453fb0d32c69a32793c166beaf6399281
NICK FURNISS <robnfurniss@gmail.com>
2021-05-20 12:00:35 +0100

    Camera rumble when near ash cloud

---
+++ Assets/Scripts/Bluetack/CameraProximityRumbler.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraProximityRumbler : MonoBehaviour
+{
+
+    private CameraController cc_cam;
+    private float f_time;
+
+
+    private void Start()
+    {
+        cc_cam = FindObjectOfType<CameraController>();
+    }
+
+    private void Update()
+    {
+        f_time += Time.deltaTime;
+
+        if (f_time > 0.1f)
+        {
+            CamRumble();
+            f_time = 0;
+        }
+    }
+
+    private void CamRumble()
+    {
+        float dist = transform.position.z + 1000 - cc_cam.transform.position.z;
+        //print("Rumbling " + (100 - Mathf.Abs(dist)) * 0.001f);
+
+        if (Mathf.Abs(dist) < 100)
+            cc_cam.Recoil((100 - Mathf.Abs(dist)) * 0.001f, 0.1f);
+    }
+}
