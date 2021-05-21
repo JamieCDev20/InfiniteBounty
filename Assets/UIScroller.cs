48d6240a17e6453ae2405c0d036e9b415ebfad34
NICK FURNISS <robnfurniss@gmail.com>
2021-05-21 13:59:42 +0100

    Retirement scene work, not finished

---
+++ Assets/UIScroller.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UIScroller : MonoBehaviour
+{
+    [SerializeField] private float f_scrollSpeed;
+
+    void Update()
+    {
+        transform.localPosition += Vector3.up * Time.deltaTime * f_scrollSpeed;
+    }
+}
